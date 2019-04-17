78ecfa63f38fef55b94b2088d15ed3de75f1c188
Lucas <lucasveberdebrida@gmail.com>
2019-04-16 22:56:45 -0300

    Ajustando script do banco

--- server/database/migrations/20190417015149-create-users.js
+++ server/database/migrations/20190417015149-create-users.js
@@ -1,35 +1,33 @@
-'use strict';
-var DataTypes = require('sequelize/lib/data-types');
+"use strict";
+var DataTypes = require("sequelize/lib/data-types");
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
+    return queryInterface.createTable("Users", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: DataTypes.INTEGER,
+        type: DataTypes.INTEGER
       },
       name: {
         allowNull: false,
-        type: DataTypes.STRING,
+        type: DataTypes.STRING
       },
       email: {
         allowNull: false,
         type: DataTypes.STRING,
-        unique: true,
+        unique: true
       },
       password: {
         allowNull: false,
-        type: DataTypes.STRING,
+        type: DataTypes.STRING
       },
-      createdAt: {
-        allowNull: false,
-        type: DataTypes.DATE,
-      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE
     });
   },
 
   down: (queryInterface, Sequelize) => {
-      return queryInterface.dropTable('users'); 
+    return queryInterface.dropTable("users");
   }
 };
