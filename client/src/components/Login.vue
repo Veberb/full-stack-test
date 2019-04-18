84a27f6a88eaa27a1414322f306a3b5d90f1ee1f
Lucas <lucasveberdebrida@gmail.com>
2019-04-17 22:04:17 -0300

    Tela de login

---
+++ client/src/components/Login.vue
@@ -0,0 +1,62 @@
+<template>
+  <b-card class="container login">
+    <img class="image" src="@/assets/mytapp-logo-12.png">
+    <h2>Login</h2>
+    <b-form @submit="onSubmit">
+      <b-form-group id="input-group-1">
+        <b-form-input
+          v-validate="'required|email'"
+          name="email"
+          autocomplete="off"
+          v-model="form.email"
+          placeholder="Email"
+        ></b-form-input>
+        <span v-show="errors.has('email')">{{ errors.first('email') }}</span>
+      </b-form-group>
+
+      <b-form-group id="input-group-2">
+        <b-form-input
+          autocomplete="off"
+          name="password"
+          type="password"
+          v-validate="'required'"
+          v-model="form.password"
+          placeholder="Senha"
+        ></b-form-input>
+        <span v-show="errors.has('password')">{{ errors.first('password') }}</span>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      form: {
+        email: undefined,
+        password: undefined
+      }
+    }
+  },
+  methods: {
+    onSubmit: function () {
+      console.log('oi')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.login {
+  max-width: 450px;
+  background-color: #e6e6e6;
+}
+.image {
+  width: 100%;
+}
+</style>
