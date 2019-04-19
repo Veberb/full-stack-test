43df86e1d22f97484d401b9526a7270379778153
Lucas <lucasveberdebrida@gmail.com>
2019-04-19 10:14:00 -0300

    Adicionando rota da listagem de cervejas

---
+++ client/src/components/Beer.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-card class="container login">oi</b-card>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      form: {
+        email: undefined,
+        password: undefined
+      }
+    }
+  },
+  methods: {
+    onSubmit: function () {
+      console.log('oi')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.login {
+  max-width: 450px;
+  background-color: #e6e6e6;
+}
+.image {
+  width: 100%;
+}
+</style>
